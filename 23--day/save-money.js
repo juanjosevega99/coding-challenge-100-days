a621542d8aadfb79c3bd48e7fc777e9b442d215c
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-03 23:34:26 -0500

    day 23

--- 23--day/save-money.js
+++
@@ -1,9 +0,0 @@
-function finalAmount(capital, time, rate) {
-  return capital * Math.pow(1 + rate, time)
-}
-
-const capital = parseFloat(prompt('How much are you going to save this month?'))
-const time = parseInt(prompt('For how many months?'))
-const interest = parseFloat(prompt('How much is the monthly interest (0 - 100%)')) / 100
-
-alert(`In ${time} month(s) you gonna save $${finalAmount(capital, time, interest).toFixed(2)}`)
