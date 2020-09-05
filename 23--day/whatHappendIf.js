f09f208d443868ffc2152e32026078180717e75e
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-04 22:48:12 -0500

    delete folders

--- 23--day/whatHappendIf.js
+++
@@ -1,9 +0,0 @@
-function finalAmount(capital, time, rate) {
-  return capital * Math.pow(1 + rate, time)
-}
-
-const capital = parseFloat(prompt('How many do you save this month: '))
-const time = parseInt(prompt('How many months: '))
-const interest = parseFloat(prompt('How many is the interest for month (0 - 100%) ')) / 100
-
-alert(`In ${time} months do you save $${finalAmount(capital, time, interest).toFixed(2)}`)
--- 23--day/whatHappendIf.py
+++
@@ -1,9 +0,0 @@
-def finalAmount(capital, time, rate):
-    return capital * (1 + rate) ** time 
-
-
-capital = float(input('How much are you going to save this month: '))
-time = float(input('For how many months: '))
-interest = float(input('How much is the monthly interest (0 - 100%) '))/100
-
-print('In {} month(s) gonna save ${}'.format(time, round(finalAmount(capital, time, interest), 2)))
