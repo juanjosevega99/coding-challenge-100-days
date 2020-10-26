b24bed30970cfbf8a423f8852bdd442674860394
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-25 23:38:57 -0500

    library 39 day

---
+++ 39-day/ThrowDices.js
@@ -0,0 +1,5 @@
+import throwDices from "./library";
+
+console.log(throwDices(6));
+console.log(throwDices(4));
+console.log(throwDices(10));
---
+++ 39-day/library.js
@@ -0,0 +1,9 @@
+const throwDices = (num) => {
+  let results = new Array();
+  for (let i = 0; i < array.length; i++) {
+    results.push(Math.ceil(Math.random() * 6));
+  }
+  return results;
+};
+
+export default throwDices;
